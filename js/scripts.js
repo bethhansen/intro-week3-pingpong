3255df211d42bfdf87298857603b1651e542d7ac
beth97209@gmail.com <Guest@Epicodus-10F.local>
2017-07-14 11:00:15 -0700

    add first condition for a false answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,19 @@
-// business logic
+$(document).ready(function() {
+  $("form#ping-pong").submit(function(event) {
+    event.preventDefault();
 var pingPong = function(number) {
-  if ((number / 15 === 0) && (number / 5 !== 0) || (number / 3 === 0)) {
+  if (number % 15 === 0)  {
      return true;
    } else {
      return false;
    }
- };
+};
 
-
-// user interface logic
-$(document).ready(function() {
-  $("form#ping-pong").submit(function(event) {
-    event.preventDefault();
     var number = parseInt($("input#number").val());
     var result = pingPong(number);
 
     $(".result").text(number);
 
-    $(".userReturn").show();
-  });
+    $("#result").show();
+});
 });
