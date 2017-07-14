0a4116aad79292c56ca4724f385afecea4b56177
beth97209@gmail.com <Guest@Epicodus-10F.local>
2017-07-14 13:37:24 -0700

    add seperation of business logic and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,31 @@
-
-var pingPong = function(number) {
-  var result = [];
-  for (var i = 1; i<= number; i ++) {
-  if (i % 15 === 0) {
-    result.push("Ping-Pong");
-  } else if (i % 5 === 0) {
-    result.push("Pong");
-  } else if (i % 3 === 0) {
-    result.push("Ping");
-    } else {
-    result.push(i);
+//---business logic------------
+$(document).ready(function() {  //  calls the document to be ready to do some jquery functions
+  var pingPong = function(number) {  //make a variable out of the function number
+  var result = [];  //make a variable that will output an array
+  for (var i = 1; i<= number; i ++) {  //for loop of var index that begins at 1
+  if (i % 15 === 0) {  // if index can be divided by 15 it will equal to false (which is less than 1)
+    result.push("Ping-Pong"); // if a number divisable by 15 if false return Ping-Pong
+  } else if (i % 5 === 0) {  // if index can be divided by 5 it will equal to false (which is less than 1)
+    result.push("Pong");  // if a number divisable by 5 if false return Ping-Pong
+  } else if (i % 3 === 0) {  //if index can be divided by 3 it will equal to false (which is less than 1)
+    result.push("Ping");  // if a number divisable by 3 if false return Ping-Pong
+  } else {  // all other numbers that dont match the following criteria
+    result.push(i);  // return the number that was entered
     }
   }
-    return result;
+    return result;  //display the result
   };
-  $(document).ready(function() {
-    $("form#pingPong").submit(function(event) {
-   var number = parseInt($("input#number").val());
-   if (isNaN(number)) {
-     alert("Please enter a number!");
-   } else {
-     var result = pingPong(number);
-     result.forEach(function(entry){
-       $("#here").append("<li>" + entry + "</li>")
-     });
-}
-event.preventDefault();
-});
+  //---user interface------------
+  $("form#pingPong").submit(function(event) {  //  find the form pingPong and when submit is pushed do this event
+   var number = parseInt($("input#number").val());  //  the variable number will take the numaric value of the input
+   if (isNaN(number)) {  //  if a number is not entered into our number function
+     alert("Please enter a number!");  //  alert the user to enter a number
+   } else {  //  if a number is entered
+   var result = pingPong(number);  // a new variable will be created with the variable pingPong
+   result.forEach(function(entry){  // the results will be added to the new function entry
+     $("#display").append("<li>" + entry + "</li>")  //  dislplay will create a list
+   });
+  }
+  event.preventDefault();  //  stops the appliction from automatically resubmitting
+  });
 });
