2708eff596b2b070f3bcf7a9cdf81a6047bcaebe
beth97209@gmail.com <Guest@Epicodus-10F.local>
2017-07-14 12:56:18 -0700

    add final working touchs

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,30 @@
-$(document).ready(function() {
-  $("form#ping-pong").submit(function(event) {
-    event.preventDefault();
 
 var pingPong = function(number) {
   var result = [];
-  for (var i = 1; i<= number; i ++)
-  if (number % 15 === 0) {
+  for (var i = 1; i<= number; i ++) {
+  if (i % 15 === 0) {
     result.push("Ping-Pong");
-    } else if (number % 5 === 0) {
+  } else if (i % 5 === 0) {
     result.push("Pong");
-    } else if (number % 3 === 0) {
+  } else if (i % 3 === 0) {
     result.push("Ping");
-    } else
+    } else {
     result.push(i);
     }
+  }
     return result;
   };
+  $(document).ready(function() {
+    $("form#pingPong").submit(function(event) {
    var number = parseInt($("input#number").val());
    if (isNaN(number)) {
-     alert("Please enter a number!")
+     alert("Please enter a number!");
    } else {
      var result = pingPong(number);
      result.forEach(function(entry){
-       $("#result").append("<li>" + "</li>")
-     }
-   }
-  });
-//
-//   $("#result").show();
-// });
-
-alert("hi");
+       $("#here").append("<li>" + entry + "</li>")
+     });
+}
+event.preventDefault();
+});
+});
