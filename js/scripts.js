812b219648039ed6ea1348b032dc2e79189942b7
Beth <beth97209@gmail.com>
2017-07-21 14:46:22 -0700

    add correct indenting for js

--- js/scripts.js
+++ js/scripts.js
@@ -15,18 +15,19 @@
     return result;  //display the result
   };
   //---user interface logic------------
-  $(document).ready(function() {  //  calls the document to be ready to do some jquery functions
-    $("form#pingPong").submit(function(event) {  //  find the form pingPong and when submit is pushed do this event
-      event.preventDefault();  //  stops the appliction from automatically resubmitting
-       var number = parseInt($("input#number").val());  //  the variable number will take the numaric value of the input
-       if (isNaN(number)) {  //  if a number is not entered into our number function
-       alert("Please enter a number!");  //  alert the user to enter a number
-       } else {  //  if a number is entered
-       var result = pingPong(number);  // a new variable will be created with the variable pingPong
-       result.forEach(function(entry){  // the results will be added to the new function entry
-       $("#display").append("<li>" + entry + "</li>");  //  dislplay will create a list
-       $("#pingPong").trigger('reset');  //  resets the form back to empty
-    });
+$(document).ready(function() {  //  calls the document to be ready to do some jquery functions
+  $("form#pingPong").submit(function(event) {  //  find the form pingPong and when submit is pushed do this event
+    event.preventDefault();  //  stops the appliction from automatically resubmitting
+      var number = parseInt($("input#number").val());  //  the variable number will take the numaric value of the input
+      if (isNaN(number)) {  //  if a number is not entered into our number function
+      alert("Please enter a number!");  //  alert the user to enter a number
+      } else {  //  if a number is entered
+      var result = pingPong(number);  // a new variable will be created with the variable pingPong
+      result.forEach(function(entry){  // the results will be added to the new function entry
+      $("#display").text(result);  //  dislplay will create a list
+      $("#pingPong").trigger('reset');  //  resets the form back to empty
+      $(".returnNumber").show(result);
+      });
     }
   });
 });
